c19d406fc32e4a7350cbc88e0322d494b6acb640
nightwinge <134153729+nightwinge@users.noreply.github.com>
2023-05-20 18:09:09 +0300

    Update variables.js

--- src/data/variables.js
+++ src/data/variables.js
@@ -1,12 +1,12 @@
 export default {
 
-    userId: "642752306441617417",
-    github: "justnyde",
-    description: "My name is Just Nyde. I am 16 years old Turkish student. I interested in Software and Design. I can develop front-end and back-end websites for now.",
+    userId: "822054116083236864",
+    github: "nightwinge",
+    description: "My name is Nightwing. I am 18 years old Turkish student. I interested in Software and Design. I can develop front-end and back-end websites for now.",
     about: {
-        description: "Hello. My name is <span class='text-primary'>Just Nyde</span>. I am <span class='text-primary'>16 years</span> old. I am a <span class='text-primary'>3rd year student</span> and I interested in <span class='text-primary'>Software</span>. I've been coding <span class='text-primary'>since 2019</span> and I want to improve myself in this field.",
-        liveIn: "İstanbul, Küçükçekmece",
-        birthday: "16 October, 2006",
+        description: "Hello. My name is <span class='text-primary'>Nightwing</span>. I am <span class='text-primary'>18 years</span> old. I am a <span class='text-primary'>3rd year student</span> and I interested in <span class='text-primary'>Software</span>. I've been coding <span class='text-primary'>since 2019</span> and I want to improve myself in this field.",
+        liveIn: "İstanbul, Kartal",
+        birthday: "22 May, 2005",
         gender: "Man"
     }
 
